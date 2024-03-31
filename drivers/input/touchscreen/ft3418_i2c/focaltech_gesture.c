48cda9655407aa369658aed5d63e34bb19221008
GuidixX <guidixm@gmail.com>
2024-03-31 22:14:04 +0300

    Revert "touchscreen: ft3418_i2c: Simplify event key reporting"
    This reverts commit 678eaf16370d04cf4a096640624d7e7afe471a26.

--- drivers/input/touchscreen/ft3418_i2c/focaltech_gesture.c
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_gesture.c
@@ -311,13 +311,19 @@ static void fts_gesture_report(struct input_dev *input_dev, int gesture_id)
         break;
     }
     /* report event key */
-    if (gesture != 1) {
+    if ((gesture == KEY_GESTURE_AOD) && fts_data->aod_changed ){
         FTS_INFO("Gesture Code=%d", gesture);
         input_report_key(input_dev, gesture, 1);
         input_sync(input_dev);
         input_report_key(input_dev, gesture, 0);
         input_sync(input_dev);
-    }
+	} else if ((gesture == KEY_GESTURE_U) && fts_data->gesture_mode) {
+        FTS_INFO("Gesture Code=%d", gesture);
+        input_report_key(input_dev, gesture, 1);
+        input_sync(input_dev);
+        input_report_key(input_dev, gesture, 0);
+        input_sync(input_dev);
+	}
 }
 
 /*****************************************************************************
